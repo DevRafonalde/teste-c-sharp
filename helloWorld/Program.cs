795509e8893435bcdee8149e51e07b3aee198579
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-07-13 16:26:30 -0300

    Hello World
    O primeiro Hello World em C#

---
+++ helloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace helloWorld {
+    public class Program {
+        public static void Main(string[] args) {
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine("Teste " + (i + 1) + "\n");
+            }
+        }
+    }
+}
