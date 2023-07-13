cbff874b7ea2b08a1c6e3eb4cd617b7f5ab05a26
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-07-13 16:35:45 -0300

    Update Program.cs

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -2,7 +2,7 @@
     public class Program {
         public static void Main(string[] args) {
             for (int i = 0; i < 10; i++) {
-                Console.WriteLine("Teste " + (i + 1) + "\n");
+                Console.WriteLine("oi");
             }
         }
     }
